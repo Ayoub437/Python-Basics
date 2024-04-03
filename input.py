cd51dc339ded7908c4fb7bcc84dfc4ffc7ea6baf
ouata <ayoub.programmierwelt@gmail.com>
2024-04-03 18:44:24 +0200

    Python basics: data types, variables and print- and input function

---
+++ input.py
@@ -0,0 +1,47 @@
+# In this file I learn the input-function
+
+# Through these parentheses the input-funtion will be executed.
+# The input-function stops the other print command.
+# As soon as the input command is executed, the program stops and wait for an input from the user in the console.
+# If I assign the input-function to a variable, then I use that variable in the print-command.
+# Die Eingabe tätige ich in der Konsole.
+print("Eating")
+time = input()
+print("I ate my lunch " + time)
+input()
+
+print("Now I am talking about me.")
+name = input()
+print("My name is " + name)
+input()
+
+print("To do list")
+today = input()
+print("The things I wanted to do is done " + today)
+input()
+
+print("Hello")
+city = input()
+print("I just wanna tell you " + city)
+input()
+
+# More exercises with input function
+print("Hello dear class")
+presentation = input()
+print("Today I will give a presentation " + presentation)
+input()
+
+# Sample tasks
+print("I wanna make a party")
+drinks = input()
+print("This week I bought " + drinks)
+input()
+
+print("New line")
+today = input()
+print("Talking about the things I wanna do today " + today)
+input()
+
+print("Universe")
+universe = input()
+print("The universe is very " + universe)
---
+++ main.py
@@ -0,0 +1,80 @@
+# Print is a function
+# These all are print commands
+# I define variables
+# Computer needs the data types to categorize and use the given values.
+x = 9
+print("Ayoub")
+print(2 * x)
+
+# Multiply
+x = 5
+print(5 * x)
+
+y = 10
+print(56 * y)
+
+# Subtract
+# Using floats with different types of operators
+k = 3.44
+print(44-k)
+print((3.658 + 4.21) * (54.23 - 5.23))
+z = 0.5
+print(30 - z)
+
+kk = 44
+print(60 - kk)
+
+sub = 100
+print(30 - sub)
+
+# Calculate with different types of operators
+print(23 - 13)
+print(20 - 10 * 3 + 50)
+print((80 - 40) + 25 * 2 - (15 + 90))
+print((55 - 22) * (31 - 2))
+
+# Playing with variables and operants
+name = "Ayoub"
+print("My name is " + name)
+
+school = "Rackow-Schule"
+Year = "2021"
+Graduation = "Fachabitur"
+print("In " + Year + " I did my " + Graduation + " in the " + school)
+
+Day = "Today the 27th"
+Employer = "Bechtle GmbH."
+print(Day + " I have a meeting with " + Employer)
+
+print("The city where i wanna live is called 'Dubai' and my second favorite city is 'Sydney'.")
+
+age = "29"
+print("he is approximately " + age + " years old.")
+
+# Da ich hier strings als Datentyp verwende, werden die strings miteinander verkettet. The values are chained together.
+print("Hallo" + " Welt")
+print("You are also capable of programming " + "games with Python.")
+
+number = "48"
+print("She always wears the " + number + " to show the stadium who she is.")
+
+products = "t-shirts, pullovers and shoes."
+Shopping = "Zalando"
+Shopping2 = "New Yorker"
+city = "Frankfurt"
+print("Yesterday I went to " + Shopping + " and bought some " + products + " After that i did the same thing in " +
+      Shopping2 + " in " + city)
+
+city = "Frankfurt am Main."
+print("I am originally from " + city)
+
+family = "Ouataleb"
+land = "Germany."
+print("His last name is " + family + " and lives in " + land)
+
+incident = "traffic accident."
+student = "Thomas"
+year = "2019"
+print("In the " + year + " the student " + student + " and his friends had a " + incident)
+
+print(name)
