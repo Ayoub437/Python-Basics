f2968cf4c30768d8d04cf7a2e2aa03fba59d4355
ouata <ayoub.programmierwelt@gmail.com>
2024-04-14 21:18:41 +0200

    Der Zuweisungsoperator

---
+++ operators.py
@@ -0,0 +1,8 @@
+# An important note: Der Zuweisungsoperator (=) arbeitet immer von rechts nach links.
+
+# Example: Der Operant rechts wird immer dem Operand links zugewiesen.
+result1 = 30
+
+# Der komplexe Ausdruck rechts wird zunächst ausgewertet, dann kommt ein Ergebnis raus
+# und wird dann dieser Variable zugewiesen.
+result2 = (30 + 3) * 3
