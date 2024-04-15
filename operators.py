bd550e68b6b581570d289ce6f0abc9ec32e24883
ouata <ayoub.programmierwelt@gmail.com>
2024-04-15 22:17:01 +0200

    Arithmetic operators

--- operators.py
+++ operators.py
@@ -6,3 +6,14 @@ result1 = 30
 # Der komplexe Ausdruck rechts wird zunächst ausgewertet, dann kommt ein Ergebnis raus
 # und wird dann dieser Variable zugewiesen.
 result2 = (30 + 3) * 3
+
+
+# Arithmetic operators
+# That is integer division (Division ohne Rest).
+print(13 // 3)  # In the console is printed 4.
+
+# Modulo operator (Rest der nach Division übrig bleibt)
+print(32 % 3)  # In console is printed 2.
+
+# Exponentation
+print(3 ** 3)  # That means 3 hoch 3.
