76c2217b7def44dd42ae5a932c3c3aabaeb00598
ouata <ayoub.programmierwelt@gmail.com>
2024-04-17 00:50:31 +0200

    Kombinierter Zuweisungsoperator/ Assignment operator

--- operators.py
+++ operators.py
@@ -17,3 +17,29 @@ print(32 % 3)  # In console is printed 2.
 
 # Exponentation
 print(3 ** 3)  # That means 3 hoch 3.
+
+
+variable1 = 3
+variable1 = variable1 + 10
+print(variable1)
+
+# Defining variables
+variable2 = 6
+variable3 = 7
+variable4 = 3 + 30
+
+# Kombinierter Zuweisungsoperator/ Assignment operator
+# When do I use that? A variable has stored a value, I want to execute an
+# operation on that value and store the new value in the same variable.
+# In variable1 is stored the "Ausdruck" variable1 + 10. Variable1 is already defined
+variable1 += 10
+print(variable1)
+
+variable2 += 14
+print(variable2)
+
+variable3 += 9
+print(variable3)
+
+variable4 += 3
+print(variable4)
