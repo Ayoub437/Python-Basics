06c7f95631ae9bd54148ce39f3f405e01678d92c
ouata <ayoub.programmierwelt@gmail.com>
2024-04-18 15:34:44 +0200

    Logical operators

--- operators.py
+++ operators.py
@@ -52,3 +52,17 @@ print(10 == 10)  # True
 print(4 == 5)  # False
 
 print(type(3 >= 1))
+
+
+# Logical operators
+# Why do I need that type of operator? To relate several comparisons to each other/
+# Um mehrere Vergleiche miteinander in Beziehung zu setzen
+# These are expressions(= Ausdrücke) and that´s why you get
+# a truth value(=Wahrheitswert) like True or False.
+# In that logical or case, if just one of these expressions
+# is True, the logical or is going to be True as well.
+# And if both expressions are True, same result
+# "or" => logisches ODER       Beispiel: (a < b) or (c == d)
+# "and" => logisches UND       Beispiel: (a < b) and (c == d)
+# If the claim is True, the logical not is going to be False.
+# "not" => logisches NICHT     Beispiel: not b
