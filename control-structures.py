af27bc3873baa0866fd5478276afa57c4e2f6d06
ouata <ayoub.programmierwelt@gmail.com>
2024-05-26 14:01:41 +0200

    The if-statement

---
+++ control-structures.py
@@ -0,0 +1,13 @@
+# Fallunterscheidungen/Verzweigungen:
+# In einer Variable etwas speichern, dass der Nutzer in das Programm eingeben kann. Er soll eine Ganzzahl eingeben.
+number = int(input("Bitte gebe eine Ganzzahl ein: "))
+
+# Dieses "if" ist ein Schlüsselwort. Nach jedem Schlüsselwort kommt eine Bedingung.
+# Diesen Codeblock nennt man Anweisungskörper oder if-Block.
+if number < 10:  # "number < 10" ist eine Bedingung.
+    print("Die eingegebene Zahl ist kleiner als 10.")
+
+# Es kann ein direkter Wahrheitswert True oder False als Bedingung drinne stehen. Z.b. if True:
+# Oder auch eine Variable. Z.b. variable = False -- if variable:
+# Wichtig ist, dass bei der Auswertung der Bedingung durch den Computer ein Wahrheitswert das Resultat sein muss.
+# Das Ergebnis der Bedingung muss entweder True oder False sein.
