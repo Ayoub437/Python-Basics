c85ef802cf2bda722b6aa78817462eb0c7778164
ouata <ayoub.programmierwelt@gmail.com>
2024-05-31 13:22:56 +0200

    Just formatted.

--- constrol-structure-loops.py
+++ constrol-structure-loops.py
@@ -6,7 +6,7 @@
 # Dieses Beispiel ist eine Endlosschleife, da die Schleife unendlich oft ausgeführt wird.
 # Eine Schleife besteht immer aus einem Schleifenkopf.
 # Bei Schleifen hat man Vergleiche als Bedingung stehen. -> "2 < 10"
-# Da ich zwei Konstanten habe miteinander vergleiche, wird die Schleifenbedingung immer True sein.
+# Da ich zwei Konstanten habe die ich miteinander vergleiche, wird die Schleifenbedingung immer True sein.
 # Wenn der Schleifenkopf True ist wird der Schleifenkörper ausgeführt. Danach wird wieder geprüft, ob der Schleifenkopf
 # True ist. Das geht immer so weiter.
 # while 2 < 10:  # "while 2 < 10:" -> Das ist ein Schleifenkopf. In diesem Schleifenkopf ist die Bedingung definiert.
