f60f31de7a9fef6a5a967fbdb74af5c887e6a72a
ouata <ayoub.programmierwelt@gmail.com>
2024-06-14 16:58:37 +0200

    The keywords break and continue and else-branch can be used in for-loops.

--- For-loops.py
+++ For-loops.py
@@ -2,6 +2,8 @@
 # Mithilfe einer for-Schleife über ein iterierbares (= wiederholendes) Objekt laufen.
 # Die for-Schleife in Kombination mit der Funktion range() als Zählerschleife einsetzen.
 # Wann benötige ich eine for-Schleife? Wenn ich eine gewisse Anzahl an Wiederholungen vorgeben möchte.
+# Die Schlüsselwörter break und continue können auch bei der for-Schleife, sei es Variante 1 oder 2, verwendet werden.
+# Der else-Zweig kann wie bei der while-Schleife am Ende angehängt werden.
 
 # Variante 1:
 # Der schleifenkopf ist die erste Zeile.
